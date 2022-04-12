2045d20806c9ea61f3c1277d7b87e71c355a62eb
JhonVe7 <jhonavelascor@gmail.com>
2022-04-12 08:51:09 -0500

    primera Actualización

---
+++ JS/forms.js
@@ -0,0 +1,13 @@
+/*
+var aux = document.getElementById("seleccion_credito")
+switch (aux) {
+    case cuota-fija:
+        console.log("<h1>cuita fija</h1>");
+        break;
+    case cuota-gracia:
+        
+        console.log("<h1>cuota de gracia</h1>");
+        break;
+    default:
+        break;
+}*/
