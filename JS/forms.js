36bf89dfbeb0e9e7e5a340354272a12a21216174
JhonVe7 <jhonavelascor@gmail.com>
2022-04-13 18:19:43 -0500

    Generador de Factura

--- JS/forms.js
+++ JS/forms.js
@@ -5,7 +5,7 @@ var aux = document.getElementById("seleccion_credito")
 switch (aux) {
     case cuota-fija:
         console.log("<h1>cuita fija</h1>");
-        break;
+        break;  
     case cuota-gracia:
         
         console.log("<h1>cuota de gracia</h1>");
